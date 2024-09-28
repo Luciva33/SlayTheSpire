00fd3157606e19b1db5c9daa9eb979f6d030f16b
luciva33 <luciva0303@gmail.com>
2024-09-28 20:41:36 +0900

    first

---
+++ Assets/Scripts/CardObj.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CardObj : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI nameText;
+    [SerializeField] TextMeshProUGUI descriptionText;
+    [SerializeField] Image icon;
+    [SerializeField] TextMeshProUGUI CostText;
+
+}
